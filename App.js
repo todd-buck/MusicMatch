b838347d6edc8c4412c27b6dee6e5096240a7219
todd-buck <btbuckjr@gmail.com>
2023-01-13 23:06:42 -0600

    initial project scaffolding

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import StackNavigator from './StackNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    // <StackNavigator></StackNavigator>
+    <View className="flex-1 items-center justify-center bg-black">
+      <FontAwesome name={'plus'} size={40} color="white"/>
+      <Text className="text-zinc-50 text-lg mt-3">Music Match</Text>
+      <Text className="text-zinc-50">An App by Todd Buck</Text>
+      <Button title='Click Me!'/>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>I am the Home Screen!</Text>
+        </View>
+    )
+}
+
+export default HomeScreen
+
+
